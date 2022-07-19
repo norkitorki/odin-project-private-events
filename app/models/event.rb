3c3e1a6a00e7be98bf6dd633c59ef16c55f99460
norkitorki <norki@tuta.io>
2022-07-19 13:51:34 +0200

    Add basic validation to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,18 @@
 class Event < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :date, presence: true
+  validates :location, presence: true 
+
+  validate :check_date_validity
+
   belongs_to :host, class_name: 'User', foreign_key: :user_id
 
   has_many :event_visitors
   has_many :attendees, through: :event_visitors, source: :user, dependent: :destroy
+
+  private
+
+  def check_date_validity
+    date && date < Time.zone.now ? errors.add(:date, "can't be in the past") : true
+  end
 end
