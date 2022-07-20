14f7c16ee6c419ec90d003b614d222504c4a1b20
norkitorki <norki@tuta.io>
2022-07-20 08:19:02 +0200

    Add username model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :username, presence: true, uniqueness: true, length: { maximum: 40 }
+
   has_many :events,
     dependent: :destroy
      
