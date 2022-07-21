83105b36672c01d6f56c7a704329daf612b84b62
norkitorki <norki@tuta.io>
2022-07-21 08:00:32 +0200

    Add update action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,17 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def update
+    return add_attendee if params[:commit] == 'Enter'
+    return remove_attendee if params[:commit] == 'Leave'
+
+    if @event.update(event_params)
+      redirect_to root_path, notice: 'Event has been successfully updated.'
+    else
+      flash.now[:alert] = 'Event has not been updated.'
+      render root_path, status: :unprocessable_entity
+    end
+  end
+
 end
