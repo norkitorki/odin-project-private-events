9a4bef8e08271fb1cb1daceb4a91596da9221169
norkitorki <norki@tuta.io>
2022-08-16 11:55:54 +0200

    Define @events and @users in show action

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,7 @@
 class SearchController < ApplicationController
   def show
     @search_term = params[:s]
+    @events = Event.where('LOWER(name) LIKE :s OR LOWER(location) LIKE :s OR LOWER(description) LIKE :s', { s: "%#{@search_term.downcase}%" })
+    @users  = User.where('LOWER(username) LIKE ?', "%#{@search_term.downcase}%")
   end
 end
