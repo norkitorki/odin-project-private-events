f49090026d4727b24b8f895613cf814d0241287a
norkitorki <norki@tuta.io>
2022-08-18 08:55:54 +0200

    Validate status presence and value

--- app/models/event_invitation.rb
+++ app/models/event_invitation.rb
@@ -5,4 +5,6 @@ class EventInvitation < ApplicationRecord
 
   belongs_to :event
   belongs_to :user
+
+  validates :status, presence: true, comparison: { greater_than_or_equal_to: -1, less_than_or_equal_to: 1 }
 end
