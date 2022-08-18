709daf5c5bb66fa9223b9ac3b9cec62de5d4d17b
norkitorki <norki@tuta.io>
2022-08-18 08:53:34 +0200

    Add scopes for :rejected, :pending and :accepted status

--- app/models/event_invitation.rb
+++ app/models/event_invitation.rb
@@ -1,4 +1,8 @@
 class EventInvitation < ApplicationRecord
+  scope :rejected, -> { where(status: -1) }
+  scope :pending, -> { where(status: 0) }
+  scope :accepted, -> { where(status: 1) }
+
   belongs_to :event
   belongs_to :user
 end
