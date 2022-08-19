dd765807d8049bc67b8da00c596a7e3fd39fd7ed
norkitorki <norki@tuta.io>
2022-08-19 13:34:03 +0200

    Add a resources block for events routes and add nested event_invitations routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :users, only: :show do 
     get 'invitations', to: 'users#invitations'
   end
   
+  resources :events do
+    resources :event_invitations, except: %i[ index edit update ]
+    get 'attendees', to: 'events#attendees'
+  end
+  
   root 'events#index'
 
-  get 'users/:id', to: 'users#show', as: :user
-  get 'events/:id/attendees', to: 'events#attendees', as: :event_attendees
   get 'search/', to: 'search#show', as: :search
 end
