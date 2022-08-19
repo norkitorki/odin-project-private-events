92cfcf2cfd6ca8c2e78393b2d1509ec528ee001f
norkitorki <norki@tuta.io>
2022-08-19 13:31:37 +0200

    Remove scopes and status model validation

--- app/models/event_invitation.rb
+++ app/models/event_invitation.rb
@@ -1,10 +1,4 @@
 class EventInvitation < ApplicationRecord
-  scope :rejected, -> { where(status: -1) }
-  scope :pending, -> { where(status: 0) }
-  scope :accepted, -> { where(status: 1) }
-
   belongs_to :event
   belongs_to :user
-
-  validates :status, presence: true, comparison: { greater_than_or_equal_to: -1, less_than_or_equal_to: 1 }
 end
