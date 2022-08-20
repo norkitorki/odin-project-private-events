e4a5978decfbed3c9883c32ed12af0835f4f1d4f
norkitorki <norki@tuta.io>
2022-08-20 08:24:07 +0200

    Add user authentication filter for invitations action and remove unnecessary @user definition

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: :invitations
+
   def show
     @user = User.find(params[:id])
   end
 
   def invitations
-    @user = User.find(params[:user_id])
   end
 end
