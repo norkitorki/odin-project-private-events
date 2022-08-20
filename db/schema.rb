31871cdfc7318102faf18aa475c46ae35786f123
norkitorki <norki@tuta.io>
2022-08-20 14:48:13 +0200

    Add private column to events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_18_052106) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_20_124429) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_18_052106) do
     t.datetime "updated_at", null: false
     t.datetime "end_date"
     t.string "description"
+    t.integer "private", default: 0
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
