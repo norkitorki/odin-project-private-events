2cdb7a35e260dd25a6fa5ad429cb965448411025
norkitorki <norki@tuta.io>
2022-08-21 08:58:33 +0200

    Add event_invitations index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  root 'events#index'
+
   resources :users, only: :show
   
   resources :events do
-    resources :invitations, controller: :event_invitations, except: %i[ index edit update ]
+    resources :invitations, controller: :event_invitations, except: %i[ edit update ]
     resources :attendees, only: %i[ index create ]
     delete :attendees, to: 'attendees#destroy'
   end
-  
-  root 'events#index'
 
   get 'user/invitations', to: 'users#invitations', as: :user_invitations
   get 'search/', to: 'search#show', as: :search
